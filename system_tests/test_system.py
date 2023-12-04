0418ff3f5db6215884cf660b85b08d47a96fdc0a
Isabel Elise <80064876+isabel-elise@users.noreply.github.com>
2023-12-04 00:02:27 -0300

    Atualiza test_system.py com pequena correção

--- system_tests/test_system.py
+++ system_tests/test_system.py
@@ -5,11 +5,11 @@ class TestSystem:
     def test_form_appears_proprely(self):
         
         self.driver.get("http://localhost:3000/")
-        element = driver.find_element(By.XPATH, "/html/body/div/div/header/span/button[2]")
+        element = self.driver.find_element(By.XPATH, "/html/body/div/div/header/span/button[2]")
         element.click()
         
-        button_type = driver.find_element(By.NAME, 'selectTipo')
-        button_day = driver.find_element(By.NAME, 'selectDia')
-        title = driver.find_element(By.NAME, 'titulo')
-        button_begin = driver.find_element(By.NAME, 'horarioInicio')
-        button_end = driver.find_element(By.NAME, 'horarioFim')
+        button_type = self.driver.find_element(By.NAME, 'selectTipo')
+        button_day = self.driver.find_element(By.NAME, 'selectDia')
+        title = self.driver.find_element(By.NAME, 'titulo')
+        button_begin = self.driver.find_element(By.NAME, 'horarioInicio')
+        button_end = self.driver.find_element(By.NAME, 'horarioFim')
