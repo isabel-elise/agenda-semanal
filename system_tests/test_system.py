e5d6e4c41f5fd96197137e89639acf2730797edc
Arthur Pontes Nader <82470629+arthurnader@users.noreply.github.com>
2023-12-04 09:26:15 -0300

    Update test_system.py

--- system_tests/test_system.py
+++ system_tests/test_system.py
@@ -1,3 +1,5 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver import ActionChains
@@ -7,6 +9,8 @@ import pytest
 import time
 import numpy as np
 from PIL import Image
+
+options = Options()
 
 def add_event(driver, dia, nome, hora_inicio, hora_fim):
     
@@ -56,11 +60,12 @@ def add_alarm(driver, dia, nome, horario):
 
     create_alarm =  driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div[2]/form/button").click()
 
-@pytest.mark.usefixtures("setup")
+
 class TestSystem:
     
     def test_form_appears_proprely(self):
         
+        self.driver = webdriver.Chrome(options=options)
         self.driver.get("http://localhost:3000/")
         element = self.driver.find_element(By.XPATH, "/html/body/div/div/header/span/button[2]")
         element.click()
@@ -71,8 +76,11 @@ class TestSystem:
         button_begin = self.driver.find_element(By.NAME, 'horarioInicio')
         button_end = self.driver.find_element(By.NAME, 'horarioFim')
 
+        self.driver.close()
+        
     def test_create_event(self):
             
+        self.driver = webdriver.Chrome(options=options)
         self.driver.maximize_window()
         self.driver.get("http://localhost:3000/")
 
@@ -86,8 +94,11 @@ class TestSystem:
         assert time_event == "18:30 - 21:45"
         assert day_event == "Quarta"
 
+        self.driver.close()
+        
     def test_remove_event(self):
     
+        self.driver = webdriver.Chrome(options=options)
         self.driver.maximize_window()
         self.driver.get("http://localhost:3000/")
             
@@ -100,8 +111,11 @@ class TestSystem:
             time_event = self.driver.find_element(By.XPATH, "/html/body/div/div/div/div[3]/div/div/span[2]").text
             day_event = self.driver.find_element(By.XPATH, "/html/body/div/div/div/div[3]/span").text
 
+        self.driver.close()
+            
     def test_alert_appears_when_setting_alarm_over_event(self):
 
+        self.driver = webdriver.Chrome(options=options)
         self.driver.maximize_window()
         self.driver.get("http://localhost:3000/")
 
@@ -114,8 +128,11 @@ class TestSystem:
         
         assert alert_text == "Um alarme foi adicionado durante um evento!"
 
+        self.driver.close()
+        
     def test_clear_board(self):
     
+        self.driver = webdriver.Chrome(options=options)
         self.driver.maximize_window()
         self.driver.get("http://localhost:3000/")
         
@@ -134,12 +151,18 @@ class TestSystem:
         
         clean_board = self.driver.find_element(By.XPATH, "/html/body/div/div/header/span/button[1]").click()
     
-        ActionChains(self.driver).move_by_offset( 20, 0).perform()
+        ActionChains(self.driver).move_by_offset( 0, 20).perform()
         
+        time.sleep(2)
         self.driver.save_screenshot("clean_board.png")
         
         initial_screenshot = np.array(Image.open("initial_page.png"))
         final_screenshot = np.array(Image.open("clean_board.png"))
-        pixels_difference = np.sum(initial_screenshot - final_screenshot)
+        pixels_difference = np.sum((initial_screenshot - final_screenshot)>10)
         
-        assert pixels_difference == 0
+        significant_difference = pixels_difference > 50
+        
+        assert significant_difference == 0
+
+        self.driver.close()
+        
