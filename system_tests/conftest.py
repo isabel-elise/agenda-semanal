c638acb96347be39953f64f8855ed63993868cbc
Isabel Elise <80064876+isabel-elise@users.noreply.github.com>
2023-12-04 10:16:15 -0300

    Atualiza conftest.py

--- system_tests/conftest.py
+++ system_tests/conftest.py
@@ -11,7 +11,6 @@ def setup(request):
     options = [
     "--headless",
     "--disable-gpu",
-    "--window-size=1920,1200",
     "--ignore-certificate-errors",
     "--disable-extensions",
     "--no-sandbox",
