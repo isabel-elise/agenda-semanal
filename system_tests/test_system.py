c151a85d6707088807a690ede84f17d242b46674
Isabel Elise <80064876+isabel-elise@users.noreply.github.com>
2023-12-04 07:15:28 -0300

    Adiciona teste de sistema de remoção de evento

--- system_tests/test_system.py
+++ system_tests/test_system.py
@@ -57,3 +57,41 @@ class TestSystem:
         assert title_event.split("\n")[0] == "Festa de Aniversário"
         assert time_event == "18:30 - 21:45"
         assert day_event == "Quarta"
+
+    def test_remove_event(url):
+    
+        self.driver.maximize_window()
+        self.driver.get("http://localhost:3000/")
+            
+        dia = "Quarta"
+        nome = "Festa de Aniversário"
+        hora_inicio = "18:30"
+        hora_fim = "21:45"
+    
+        element = self.driver.find_element(By.XPATH, "/html/body/div/div/header/span/button[2]")
+        element.click()
+        
+        select_type = self.driver.find_element(By.NAME, 'selectTipo')
+        select_event = Select(select_type)
+        select_event.select_by_visible_text("Evento")
+    
+        select_day = self.driver.find_element(By.NAME, 'selectDia')
+        select_week_day = Select(select_day)
+        select_week_day.select_by_visible_text(dia)
+    
+        title_field = self.driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div[2]/form/ul/li[2]/input")
+        title_field.send_keys(nome)
+    
+        begin_field = self.driver.find_element(By.NAME, 'horarioInicio')
+        begin_field.send_keys(hora_inicio)
+    
+        end_field = self.driver.find_element(By.NAME, 'horarioFim')
+        end_field.send_keys(hora_fim)
+    
+        create_event =  self.driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div[2]/form/button").click()
+        
+        remove_field = self.driver.find_element(By.XPATH, "/html/body/div/div/div/div[3]/div/div/span[1]/span").click()
+        
+        title_event = self.driver.find_element(By.XPATH, "/html/body/div/div/div/div[3]/div/div/span[1]").text
+        time_event = self.driver.find_element(By.XPATH, "/html/body/div/div/div/div[3]/div/div/span[2]").text
+        day_event = self.driver.find_element(By.XPATH, "/html/body/div/div/div/div[3]/span").text
