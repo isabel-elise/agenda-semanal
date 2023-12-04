74eb077ba5804126d819d42b10a0aa37b1a50eea
Isabel Elise <80064876+isabel-elise@users.noreply.github.com>
2023-12-04 10:15:29 -0300

    Atualiza conftest.py

--- system_tests/conftest.py
+++ system_tests/conftest.py
@@ -10,6 +10,12 @@ def setup(request):
     chrome_options = Options()
     options = [
     "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage",
     "--disable-notifications",
     "--disable-popup-blocking",
     "--disable-web-security"
