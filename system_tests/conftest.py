4a2c99d351d9d7693733bd1873454fe9d63e2772
Isabel Elise <80064876+isabel-elise@users.noreply.github.com>
2023-12-03 23:52:53 -0300

    Atualiza conftest.py para solução de erros na CI

--- system_tests/conftest.py
+++ system_tests/conftest.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.utils import ChromeType
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver import ActionChains
@@ -11,7 +10,6 @@ from selenium.webdriver import ActionChains
 
 @pytest.fixture()
 def setup(request):
-    chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install())
 
     chrome_options = Options()
     options = [
@@ -29,7 +27,7 @@ def setup(request):
     for option in options:
         chrome_options.add_argument(option)
 
-    request.cls.driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+    request.cls.driver = webdriver.Chrome(options=chrome_options)
 
     url = "http://localhost:3000/"
 
