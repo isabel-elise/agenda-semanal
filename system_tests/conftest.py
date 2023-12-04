d259f3ab09d59161840e3e884f2bd60c3766e872
Isabel Elise <80064876+isabel-elise@users.noreply.github.com>
2023-12-04 00:07:17 -0300

    Atualiza imports nos arquivos de testes de sistema

--- system_tests/conftest.py
+++ system_tests/conftest.py
@@ -1,11 +1,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver import ActionChains
 
 
 @pytest.fixture()
